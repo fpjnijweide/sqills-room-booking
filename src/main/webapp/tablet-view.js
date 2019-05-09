a54b928b6e0e372123fa14433497a48ee7b8fead
Andrew <a.j.heath@student.utwente.nl>
2019-05-09 12:11:31 +0200

    Integrated Room booking and table feature

--- src/main/webapp/tablet-view.js
+++ src/main/webapp/tablet-view.js
@@ -1,78 +1,64 @@
 // import axios from 'axios'
-
-var myObj
-
-// document.onload = getData()
-function selectRoom(){
-    let inputfield = document.getElementById("room-input")
-    let roomnr = inputfield.value
-    let roomNumber = roomnr
-    let inputform = document.getElementById("input-form")
-    // inputform.innerHTML=""
-    getData(roomNumber)
+function selectRoom() {
+    getData(document.getElementById("room-input").value);
 }
 
-function getData(roomNumber){
-
-    axios.get(`/api/room/${roomNumber}`).then((response)=>{
-        generateTable(response.data,roomNumber);
-    })
+function getData(roomNumber) {
+    axios.get(`/api/room/${roomNumber}`).then((response) => {
+        if (!isRoomFree(response.data)) {
+            generateTable(response.data, roomNumber);
+            displayRoomIsBooked(roomNumber);
+        } else {
+            displayRoomIsFree(roomNumber);
+            showMakeBooking();
+        }
+    });
 }
 
-function roomIsFree(roomNumber){
+function displayRoomIsFree(roomNumber) {
+    let roomStatus = document.getElementById("room-status");
+    roomStatus.innerHTML = "<h3>Room " + roomNumber + " is free!</h3>"
+}
+
+function displayRoomIsBooked(roomNumber) {
     let roomStatus = document.getElementById("room-status")
-    roomStatus.innerHTML="<h3>Room " + roomNumber + " is free!</h3>"
-    // roomStatus.style.color="green"
+    roomStatus.innerHTML = "<h3>Room " + roomNumber + " is booked right now</h3>"
 }
 
-function roomIsBooked(roomNumber) {
-    let roomStatus = document.getElementById("room-status")
-    roomStatus.innerHTML="<h3>Room " + roomNumber + " is booked right now</h3>"
-    // roomStatus.style.color="red"
+function isRoomFree(tableData) {
+    for (let x in tableData) {
+        let startTimeSplit = tableData[x].startTime.split(":")
+        let endTimeSplit = tableData[x].endTime.split(":")
+        let currentDate = new Date()
+        let startDateTime = new Date()
+        let endDateTime = new Date()
+        startDateTime.setHours(startTimeSplit[0], startTimeSplit[1])
+        endDateTime.setHours(endTimeSplit[0], endTimeSplit[1])
+        if (startDateTime < currentDate && currentDate < endDateTime) {
+            return false;
+        }
+    }
+    return true;
+
 }
 
-function generateTable(tableData,roomNumber){
-    let foundCurrentBooking = false
-    let content = document.getElementById("content");
-    let currentDate = new Date()
-    let tableContent = `<table class="table" id="room-bookings">
+function generateTable(tableData, roomNumber) {
+        let content = document.getElementById("content");
+        let tableContent = `<table class="table" id="room-bookings">
                         <tr>
                          <th>Start Time</th>   
                          <th>End Time</th>   
                         </tr>`;
-
-    for (let x in tableData) {
-        let startTimeSplit = tableData[x].startTime.split(":")
-        let endTimeSplit = tableData[x].endTime.split(":")
-
-        let startDateTime = new Date()
-        let endDateTime = new Date()
-        startDateTime.setHours(startTimeSplit[0],startTimeSplit[1])
-        endDateTime.setHours(endTimeSplit[0],endTimeSplit[1])
-
-        // TODO include check if it is booked in the next half hour
-        if (startDateTime < currentDate && currentDate < endDateTime) {
-
-            foundCurrentBooking = true
-        }
-
-
-
-        tableContent += `<tr> 
+        for (let x in tableData) {
+            tableContent += `<tr> 
             <td> ${tableData[x].startTime} </td> 
             <td> ${tableData[x].endTime} </td> 
         </tr>`
+        }
+        tableContent += `</table>`;
+        content.innerHTML = tableContent;
+        setInterval(() => {
+            getData(roomNumber);
+        }, 30000)
     }
-
-    tableContent += `</table>`;
-    content.innerHTML = tableContent;
-    if (!foundCurrentBooking){
-        roomIsFree(roomNumber)
-    } else {
-        roomIsBooked(roomNumber)
-    }
-    setInterval(() => {
-        getData(roomNumber);
-    }, 30000)
-}
 
