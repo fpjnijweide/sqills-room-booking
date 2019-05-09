0152a1a5e73fc8943d1dd36a6214ae47db72a225
fpjnijweide <fpjnijweide@protonmail.com>
2019-05-09 10:59:31 +0200

    implemented check for if room is currently booked

--- src/main/webapp/tablet-view.js
+++ src/main/webapp/tablet-view.js
@@ -12,10 +12,30 @@ function getData(){
     })
 }
 
+function roomisBooked(roomNumber){
+    // TODO implement
+}
+
 function generateTable(tableData){
     let table = document.getElementById("room-bookings")
+    let currentDate = new Date()
     let txt = ""
     for (let x in tableData) {
+        let startTimeSplit = tableData[x].startTime.split(":")
+        let endTimeSplit = tableData[x].startTime.split(":")
+
+        let startDateTime = new Date()
+        let endDateTime = new Date()
+        startDateTime.setHours(startTimeSplit[0],startTimeSplit[1])
+        endDateTime.setHours(endTimeSplit[0],endTimeSplit[1])
+
+        // TODO include check if it is booked in the next half hour
+        if (startDateTime < currentDate && currentDate < endDateTime) {
+            roomisBooked(roomNumber)
+        }
+
+
+
         txt += "<tr>" +
             "<td>" + tableData[x].roomNumber + "</td>" +
             "<td>" + tableData[x].startTime + "</td>" +
