6873294a0be2d53bcb1ce973b4702d4216a6ec82
s1857746 <f.p.j.nijweide@student.utwente.nl>
2019-05-09 11:42:09 +0200

    added form for entering room number

--- src/main/webapp/tablet-view.js
+++ src/main/webapp/tablet-view.js
@@ -1,26 +1,38 @@
 // import axios from 'axios'
 
 var myObj
-var roomNumber=1
 
-document.onload = getData()
+function selectRoom(){
+    let inputfield = document.getElementById("room-input")
+    let roomnr = inputfield.value
+    let roomNumber = roomnr
+    let inputform = document.getElementById("input-form")
+    // inputform.innerHTML=""
+    getData(roomNumber)
+}
 
-function getData(){
+// document.onload = getData()
+
+function getData(roomNumber){
 
     axios.get(`/api/room/${roomNumber}`).then((response)=>{
-        generateTable(response.data);
+        generateTable(response.data,roomNumber);
     })
 }
 
 function roomIsFree(roomNumber){
-    console.log("room is free")
+    let roomStatus = document.getElementById("room-status")
+    roomStatus.innerHTML="<h3>Room " + roomNumber + " is free!</h3>"
+    // roomStatus.style.color="green"
 }
 
-function roomIsBooked(roomNumber){
-    alert("room is booked")
+function roomIsBooked(roomNumber) {
+    let roomStatus = document.getElementById("room-status")
+    roomStatus.innerHTML="<h3>Room " + roomNumber + " is booked right now</h3>"
+    // roomStatus.style.color="red"
 }
 
-function generateTable(tableData){
+function generateTable(tableData,roomNumber){
     let foundCurrentBooking = false
     let table = document.getElementById("room-bookings")
     let currentDate = new Date()
