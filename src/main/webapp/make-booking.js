5447f58ffe7905b9cb46cfad2d42a82abb31f2a3
Andrew <a.j.heath@student.utwente.nl>
2019-05-10 11:28:16 +0200

    Started improvement of front-end

--- src/main/webapp/make-booking.js
+++ src/main/webapp/make-booking.js
@@ -2,12 +2,15 @@ function showMakeBooking() {
     document.getElementById(`content`).innerHTML = `
             <input onchange="selectHowLong(value)" type="range" min="30" max="240" step="30" class="custom-range"
                id="booking-duration">
-        <button onclick="makeBooking()" class="btn btn-primary">Book</button> <div id="booking-duration-value"></div>
+        <button onclick="makeBooking()" class="btn btn-primary"><i
+                                    class="fas fa-rocket"></i></button> <div id="booking-duration-value"></div>
     `;
 }
-function selectHowLong(value){
+
+function selectHowLong(value) {
     document.getElementById("booking-duration-value").innerHTML = ` for <p>${value.toString()} minutes</p>`;
 }
+
 function makeBooking(response) {
     let duration = document.getElementById(`booking-duration`).value;
     let endTime = addMinutes(new Date(), duration);
@@ -17,12 +20,12 @@ function makeBooking(response) {
 }
 
 function addMinutes(date, minutes) {
-    return new Date(date.getTime() + minutes*60000);
+    return new Date(date.getTime() + minutes * 60000);
 }
 
 function displayBooked() {
     document.getElementById(`book-now`).innerHTML = `<h3> Booking complete </h3>`;
-        setTimeout(() => {
-            getData(currentRoomNumber);
-        }, 5000);
+    setTimeout(() => {
+        getData(currentRoomNumber);
+    }, 5000);
 }
