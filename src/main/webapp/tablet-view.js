362417bf2711379119bff9b8254752342c246b9f
Andrew <a.j.heath@student.utwente.nl>
2019-05-10 10:46:59 +0200

    Fixed bug

--- src/main/webapp/tablet-view.js
+++ src/main/webapp/tablet-view.js
@@ -1,73 +1,74 @@
 // TODO remove this hardcoded variable later
 var totalNumberOfRooms = 5;
 var refreshSet = false;
+var currentRoomNumber;
 function selectRoom() {
-    getData(document.getElementById("room-input").value);
+    currentRoomNumber = document.getElementById("room-input").value
+    getData(currentRoomNumber);
     if(!refreshSet){
         setInterval(() => {
-            getData(document.getElementById("room-input").value);
+            getData(currentRoomNumber);
         }, 30000);
         refreshSet = true;
     }
 }
 
-function getData(roomNumber, checkingForCurrentRoom) {
+function getData(roomNumberInput, checkingForCurrentRoom) {
     if (typeof checkingForCurrentRoom === 'undefined') checkingForCurrentRoom = true;
-    axios.get(`/api/room/${roomNumber}`).then((response) => {
+    axios.get(`/api/room/${roomNumberInput}`).then((response) => {
         let roomStartTime = isRoomFree(response.data);
         if (checkingForCurrentRoom) {
             if (!roomStartTime) { // room is booked
-                generateTable(response.data, roomNumber);
-                displayRoomIsBooked(roomNumber);
-                checkIfOtherRoomsAreBooked(roomNumber)
+                generateTable(response.data);
+                displayRoomIsBooked();
+                checkIfOtherRoomsAreBooked()
             } else {
-                displayRoomIsFree(roomNumber, roomStartTime);
+                displayRoomIsFree(roomStartTime);
 
-                showMakeBooking(roomNumber);
+                showMakeBooking();
             }
         } else {
-            displayOtherFreeRooms(roomStartTime, roomNumber);
+            displayOtherFreeRooms(roomStartTime, roomNumberInput);
         }
     });
 }
 
-function displayOtherFreeRooms(roomStartTime, roomNumber) {
+function displayOtherFreeRooms(roomStartTime, roomNumberInput) {
     if (roomStartTime) {
         let roomStatus = document.getElementById("room-status");
         let currentTime = new Date();
         if (currentTime.getHours() <= roomStartTime.getHours() && currentTime.getMinutes() < roomStartTime.getMinutes()) {
-            roomStatus.innerHTML += `<h4>But, room   ${roomNumber}  is free until 
-                ${roomStartTime.getHours()}: ${roomStartTime.getMinutes()}!</h4>`
+            roomStatus.innerHTML += `<h4>But, room   ${roomNumberInput}  is free until 
+                ${roomStartTime.getHours()}: ${roomStartTime.getMinutes()}!</h4>`;
         } else {
-            roomStatus.innerHTML += `<h4>But, room ${roomNumber}  is free for the whole day!</h4>`
+            roomStatus.innerHTML += `<h4>But, room ${roomNumberInput}  is free for the whole day!</h4>`;
         }
 
     }
 }
 
-function checkIfOtherRoomsAreBooked(roomNumber) {
-    let roomStatus = document.getElementById("room-status")
+function checkIfOtherRoomsAreBooked() {
     for (let i = 0; i < totalNumberOfRooms; i++) { // TODO change these numbers to loop through actual rooms yes.
-        if (i !== roomNumber) {
-            getData(i, false)
+        if (i !== currentRoomNumber) {
+            getData(i, false);
         }
     }
 }
 
-function displayRoomIsFree(roomNumber, roomStartTime) {
+function displayRoomIsFree(roomStartTime) {
     let roomStatus = document.getElementById("room-status");
     let currentTime = new Date();
     if (currentTime.getHours() <= roomStartTime.getHours() && currentTime.getMinutes() < roomStartTime.getMinutes()) {
-        roomStatus.innerHTML = "<h3>Room " + roomNumber + " is free until " +
+        roomStatus.innerHTML = "<h3>Room " + currentRoomNumber + " is free until " +
             roomStartTime.getHours() + ":" + roomStartTime.getMinutes() + "!</h3>"
     } else {
-        roomStatus.innerHTML = "<h3>Room " + roomNumber + " is free for the whole day!</h3>"
+        roomStatus.innerHTML = "<h3>Room " + currentRoomNumber + " is free for the whole day!</h3>"
     }
 }
 
-function displayRoomIsBooked(roomNumber) {
+function displayRoomIsBooked() {
     let roomStatus = document.getElementById("room-status");
-    roomStatus.innerHTML = "<h3>Room " + roomNumber + " is booked right now</h3>"
+    roomStatus.innerHTML = "<h3>Room " + currentRoomNumber + " is booked right now</h3>"
 }
 
 function isRoomFree(tableData) {
@@ -93,7 +94,7 @@ function isRoomFree(tableData) {
 
 }
 
-function generateTable(tableData, roomNumber) {
+function generateTable(tableData) {
     let content = document.getElementById("content");
     let tableContent = `<table class="table" id="room-bookings">
                         <tr>
