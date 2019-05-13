3fa41f0db4ac99577cab96b9d7dfc20e0fbde59f
Marten Voorberg <martenvoorberg@gmail.com>
2019-05-13 11:10:47 +0200

    Add Javadoc to Booking API methods

--- src/main/java/nl/utwente/api/room/Booking.java
+++ src/main/java/nl/utwente/api/room/Booking.java
@@ -8,9 +8,15 @@ import javax.ws.rs.core.MediaType;
 
 @Path("/booking")
 public class Booking {
+    /**
+     * Returns a specific booking.
+     * @param bookingID ID of booking to be returned
+     * @return JSON Object for the requested booking
+     */
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     @Path("{bookingID}")
+    // Todo: Implement
     public String getSpecificBooking(@PathParam("bookingID") int bookingID) {
         final JsonNodeFactory factory = JsonNodeFactory.instance;
         ObjectNode booking = factory.objectNode();
@@ -22,9 +28,19 @@ public class Booking {
         return booking.toString();
     }
 
+    /**
+     * Creates a booking.
+     * @param roomNumber number of room in which the meeting will take place
+     * @param date date of booking
+     * @param startTime start time of booking
+     * @param endTime end time of booking
+     * @return JSON object containing a "success" boolean field specifying whether the booking was
+     *         successfully created
+     */
     @POST
     @Produces(MediaType.APPLICATION_JSON)
     @Path("/create")
+    // Todo: Implement
     public String createBooking(
         @QueryParam("roomNumber") int roomNumber,
         @QueryParam("date") String date,
@@ -37,9 +53,20 @@ public class Booking {
         return success.toString();
     }
 
+    /**
+     * Updates a specified booking.
+     * @param bookingID ID specifying the booking to be updated
+     * @param roomNumber new roomNumber of the booking
+     * @param date new date of the booking
+     * @param startTime new start time of the booking
+     * @param endTime new end time of the booking
+     * @return JSON object containing a "success" boolean field specifying whether the booking was
+     *         successfully updated
+     */
     @PUT
     @Produces(MediaType.APPLICATION_JSON)
     @Path("/{bookingID}/update")
+    // Todo: Implement
     public String createBooking(
         @PathParam("bookingID") int bookingID,
         @QueryParam("roomNumber") int roomNumber,
@@ -53,9 +80,16 @@ public class Booking {
         return success.toString();
     }
 
+    /**
+     * Deletes a specific booking
+     * @param bookingID Path parameter specifying the booking to be deleted
+     * @return JSON object containing a "success" boolean field specifying whether the booking was
+     *         successfully deleted
+     */
     @DELETE
     @Produces(MediaType.APPLICATION_JSON)
     @Path("/{bookingID}/delete")
+    // Todo: Implement
     public String createBooking(
         @PathParam("bookingID") int bookingID
     ) {
