d85e7ced28c4f82783ad27a40a72c31318087644
Marten Voorberg <martenvoorberg@gmail.com>
2019-05-16 09:12:29 +0200

    Merge test files

--- src/test/java/nl/utwente/dao/BookingDaoTest.java
+++ src/test/java/nl/utwente/dao/BookingDaoTest.java
@@ -15,13 +15,26 @@ public class BookingDaoTest {
     private Connection connection;
 
     @Before
-    public void setUpConnection() {
+    public void setUpConnectionAndPopulate() {
         this.connection = DatabaseConnectionFactory.getConnection();
+
+        try {
+            Statement statement = connection.createStatement();
+            String query = "INSERT INTO Sqills.Booking(bookingdate, starttime, endtime, roomID) " +
+                           "VALUES ('2030-12-12', '9:00:00', '10:00:00', 1)";
+            int updatedRows = statement.executeUpdate(query);
+        } catch(SQLException sqle) {
+            System.err.println(sqle);
+        }
     }
 
     @After
     public void closeConnection() {
         try {
+            Statement statement = connection.createStatement();
+            String query = "DELETE FROM sqills.Booking WHERE bookingdate = '2030-12-12'";
+            int updatedRows = statement.executeUpdate(query);
+            statement.close();
             this.connection.close();
         } catch (SQLException e) {
             e.printStackTrace();
@@ -98,5 +111,30 @@ public class BookingDaoTest {
     public void testDeleteBookingNonExisting() {
         boolean result = BookingDao.deleteBooking(-1);
         assertFalse(result);
+    }
+
+    @Test
+    public void testValidBooking(){
+        //booking for same time, date room: shoukld not be accepted
+        boolean result1 = BookingDao.isValidBooking(1, "9:00:00", "10:00:00", "2030-12-12");
+        //valid booking with startime just after endtime current booking: should be accepted
+        boolean result2 = BookingDao.isValidBooking(1, "10:00:00", "11:00:00", "2030-12-12");
+        //booking with the current booking in between: should not be accepted
+        boolean result3 = BookingDao.isValidBooking(1, "9:00:00", "11:00:00", "2030-12-12");
+        //other date same time same room: should be accepted
+        boolean result4 = BookingDao.isValidBooking(1, "9:00:00", "11:00:00", "2030-12-13");
+        //other room booking for same time: should be accepted
+        boolean result5 = BookingDao.isValidBooking(2, "9:00:00", "11:00:00", "2030-12-12");
+        //endtime before startime: should not be accepted
+        boolean result6 = BookingDao.isValidBooking(1, "13:00:00", "12:00:00", "2030-12-12");
+        //booking with endtime just before current booking starttime: should be accepted
+        boolean result7 = BookingDao.isValidBooking(1, "8:00:00", "9:00:00", "2030-12-12");
+        assertFalse(result1);
+        assertTrue(result2);
+        assertFalse(result3);
+        assertTrue(result4);
+        assertTrue(result5);
+        assertFalse(result6);
+        assertTrue(result7);
     }
 }
