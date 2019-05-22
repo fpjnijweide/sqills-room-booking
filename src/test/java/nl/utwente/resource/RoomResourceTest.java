b1ef5bc8eba24fec43aaa54b327915434cb52701
Andrew <a.j.heath@student.utwente.nl>
2019-05-22 12:04:28 +0200

    move test working :O ⢀⣠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⣠⣤⣶⣶ ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⢰⣿⣿⣿⣿ ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⣀⣀⣾⣿⣿⣿⣿ ⣿⣿⣿⣿⣿⡏⠉⠛⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⣿ ⣿⣿⣿⣿⣿⣿⠀⠀⠀⠈⠛⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠛⠉⠁⠀⣿ ⣿⣿⣿⣿⣿⣿⣧⡀⠀⠀⠀⠀⠙⠿⠿⠿⠻⠿⠿⠟⠿⠛⠉⠀⠀⠀⠀⠀⣸⣿ ⣿⣿⣿⣿⣿⣿⣿⣷⣄⠀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⣿⣿ ⣿⣿⣿⣿⣿⣿⣿⣿⣿⠏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠠⣴⣿⣿⣿⣿ ⣿⣿⣿⣿⣿⣿⣿⣿⡟⠀⠀⢰⣹⡆⠀⠀⠀⠀⠀⠀⣭⣷⠀⠀⠀⠸⣿⣿⣿⣿ ⣿⣿⣿⣿⣿⣿⣿⣿⠃⠀⠀⠈⠉⠀⠀⠤⠄⠀⠀⠀⠉⠁⠀⠀⠀⠀⢿⣿⣿⣿ ⣿⣿⣿⣿⣿⣿⣿⣿⢾⣿⣷⠀⠀⠀⠀⡠⠤⢄⠀⠀⠀⠠⣿⣿⣷⠀⢸⣿⣿⣿ ⣿⣿⣿⣿⣿⣿⣿⣿⡀⠉⠀⠀⠀⠀⠀⢄⠀⢀⠀⠀⠀⠀⠉⠉⠁⠀⠀⣿⣿⣿ ⣿⣿⣿⣿⣿⣿⣿⣿⣧⠀⠀⠀⠀⠀⠀⠀⠈⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢹⣿⣿ ⣿⣿⣿⣿⣿⣿⣿⣿⣿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿

--- src/test/java/nl/utwente/resource/RoomResourceTest.java
+++ src/test/java/nl/utwente/resource/RoomResourceTest.java
@@ -1,5 +1,7 @@
 package nl.utwente.resource;
 
+import com.google.gson.Gson;
+import nl.utwente.dao.BookingDao;
 import nl.utwente.dao.RoomDao;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.test.JerseyTest;
@@ -9,6 +11,8 @@ import javax.ws.rs.core.Application;
 import javax.ws.rs.core.Response;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
 public class RoomResourceTest extends JerseyTest {
 
     @Override
@@ -20,9 +24,24 @@ public class RoomResourceTest extends JerseyTest {
     public void listRoom(){
         Response res = target().path("/room/list").request().get();
         assertEquals("Should return status 200", 200, res.getStatus());
+        assertNotNull(res);
         assertEquals(RoomDao.getAllRooms().toString().replaceAll("\\s",""), res.readEntity(String.class));
     }
 
+    @Test
+    public void getBookingsForSpecificRoomToday(){
+        String jsonCheck = new Gson().toJson(BookingDao.getBookingsForRoomToday(1));
+        System.out.println(jsonCheck);
+//        for (int i = 0; i < 10; i++) {
+//            Response res = target().path("/room/"+i).request().get();
+//            String json = new Gson().toJson(res);
+//            assertEquals("Should return status 200", 200, res.getStatus());
+//            String jsonCheck = new Gson().toJson(BookingDao.getBookingsForRoomToday(i));
+//            System.out.println(jsonCheck);
+//            assertEquals(BookingDao.getBookingsForRoomToday(i).toString(), json);
+//
+//        }
+    }
 
 
 
