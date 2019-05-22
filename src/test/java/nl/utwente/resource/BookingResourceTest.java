5f01ac763871a405b9f492b909cbc3aeed8ae608
= <s.lute@student.utwente.nl>
2019-05-22 12:02:03 +0200

    BookingResourceTest

--- src/test/java/nl/utwente/resource/BookingResourceTest.java
+++ src/test/java/nl/utwente/resource/BookingResourceTest.java
@@ -1,31 +1,26 @@
-//package nl.utwente.resource;
-//
-//import junit.framework.TestCase;
-//import nl.utwente.dao.RoomDao;
-//import org.glassfish.jersey.server.ResourceConfig;
-//import org.glassfish.jersey.test.JerseyTest;
-//import org.glassfish.jersey.test.TestProperties;
-//import org.junit.Test;
-//
-//import javax.ws.rs.core.Application;
-//
-//public class BookingResourceTest extends JerseyTest {
-//
-//    @Override
-//    public Application BookingResourceTest(){
-//        enable(TestProperties.LOG_TRAFFIC);
-//        enable(TestProperties.DUMP_ENTITY);
-//        return new ResourceConfig(BookingResource.class);
-//    }
-//
-//    @Test
-//    public void test(){
-//        RoomDao roomDao = new RoomDao();
-//
-//
-//
-//
-//        TestCase.assertEquals(, roomDao.getAllRooms());
-//    }
-//
-//}
+package nl.utwente.resource;
+
+import nl.utwente.dao.BookingDao;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.test.JerseyTest;
+import org.junit.Test;
+
+import javax.ws.rs.core.Application;
+import javax.ws.rs.core.Response;
+
+import static org.junit.Assert.assertEquals;
+public class BookingResourceTest extends JerseyTest {
+
+    @Override
+    public Application configure(){
+        return new ResourceConfig(BookingResource.class);
+    }
+
+    @Test
+    public void bookingIDBooking(){
+        Response res = target().path("/booking/bookingID").request().get();
+        assertEquals("Should return status 200", 200, res.getStatus());
+        assertEquals(BookingDao.getSpecificBooking(1).toString().replaceAll("\\s",""), res.readEntity(String.class));
+    }
+
+}
