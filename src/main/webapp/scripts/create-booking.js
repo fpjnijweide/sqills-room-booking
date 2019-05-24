dfa95c23ee987bd4613c042707a1cdeaedee7045
Marten Voorberg <martenvoorberg@gmail.com>
2019-05-24 12:00:15 +0200

    Remove trailing log statement

--- src/main/webapp/scripts/create-booking.js
+++ src/main/webapp/scripts/create-booking.js
@@ -8,14 +8,44 @@ function createBooking() {
         "isPrivate": document.getElementById("isPrivate").checked
     };
 
-    console.log(requestBody);
-
     axios.post("/api/booking/create", requestBody)
         .then(response => {
             if (response.data.success) {
                 $("#success-modal").modal();
             } else {
                 $("#fail-modal").modal();
+            }
+        })
+        .finally(
+            updateBookingOverview()
+        );
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function updateBookingOverview() {
+    // The sleep is required because this function is called right after the
+    // POST create request is sent. We have to delay this GET request to make sure the
+    // database is actually updated.
+    await sleep(2000);
+    axios.get(`/api/room/${ROOM_ID}/week`)
+        .then(response => {
+            let element = document.getElementById("bookings-table");
+            element.innerHTML = "<tr>" +
+                                    "<th>Date</th>" +
+                                    "<th>Start Time</th>" +
+                                    "<th>End Time</th>" +
+                                "</tr>";
+
+            for (let i = 0; i < response.data.length; i++) {
+                let booking = response.data[i];
+                element.innerHTML += "<tr>" +
+                    "<td>" + booking.date + "</td>" +
+                    "<td>" + booking.startTime + "</td>" +
+                    "<td>" + booking.endTime + "</td>" +
+                "</tr>" ;
             }
         });
 }
