a9dc57f0c5d99f51c34a7a8339d7489e55f71d46
Platon Frolov <p.m.frolov@student.utwente.nl>
2019-06-03 11:13:20 +0200

    correct email autocorrect corrected

--- src/main/java/nl/utwente/resource/UserResource.java
+++ src/main/java/nl/utwente/resource/UserResource.java
@@ -4,9 +4,9 @@ import nl.utwente.dao.UserDao;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import java.util.List;
 
 @Path("/user")
 public class UserResource {
@@ -15,15 +15,15 @@ public class UserResource {
     }
 
     @GET
-    @Path("/list")
+    @Path("/{email}")
     @Produces(MediaType.APPLICATION_JSON)
     /**
      * Returns all of today's bookings for a specific room.
      * @param roomNumber Number specifying the room
      * @return JSON object containing all of today's bookings for a specific room
      */
-    public List<String> getRoomList () {
-        return UserDao.getAllUserMails();
+    public String getUserList (@PathParam("email") String email) {
+        return "{ \"email\":" + "\"" + UserDao.getEmail(email) + "\""+ "}";
     }
 
 }
