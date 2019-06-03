0e9d17ffd1bc4ccf40b771f33ebfb753aef514c9
fpjnijweide <fpjnijweide@protonmail.com>
2019-06-03 10:04:58 +0200

    changed more sql stuff

--- design/schema.sql
+++ design/schema.sql
@@ -19,20 +19,20 @@ create table sqills.room
 
 create table sqills.booking
 (
-    bookingid serial
+    bookingid serial  not null
         constraint booking_pk
             primary key,
-    date date,
-    starttime time without time zone,
-    endtime time without time zone,
-    roomid int not null
+    date      date,
+    starttime time,
+    endtime   time,
+    roomid    integer not null
         constraint booking_room_roomid_fk
             references sqills.room,
-    owner int not null
+    owner     integer not null
         constraint booking_users_userid_fk
             references sqills.users,
     isprivate boolean,
-    title varchar
+    title     varchar
 );
 
 
