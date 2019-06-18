ef6fd78ecb5541b4665a97b2f0528485a001fd38
Marten Voorberg <martenvoorberg@gmail.com>
2019-06-18 09:56:27 +0200

    Add pop up for booking details to the tablet interface

--- src/main/webapp/scripts/tablet/pop-up-controller.js
+++ src/main/webapp/scripts/tablet/pop-up-controller.js
@@ -6,10 +6,28 @@ function unblurBackground() {
     document.getElementsByClassName("blur-container")[0].classList.remove("blur");
 }
 
-function showBookingPopUp() {
+function showCreationPopUp() {
     document.getElementById("make-booking-overlay").classList.remove("hidden");
 }
 
-function hideBookingPopUp() {
+function hideCreationPopUp() {
     document.getElementById("make-booking-overlay").classList.add("hidden");
+}
+
+function openDetailsPopUp(bookingID) {
+    axios.get(`/api/booking/${bookingID}`)
+        .then(response => {
+           document.getElementById("pop-up-title").innerText = response.data.title;
+           document.getElementById("pop-up-owner").innerHTML = `<span class="booked-by">Booked by ${response.data.userName}`;
+           document.getElementById("pop-up-time").innerHTML = response.data.startTime + " - " + response.data.endTime;
+        })
+        .finally( () => {
+            document.getElementById("booking-details-overlay").classList.remove("hidden");
+            blurBackground();
+        });
+}
+
+function closeDetailsPopUp() {
+    document.getElementById("booking-details-overlay").classList.add("hidden");
+    unblurBackground();
 }
