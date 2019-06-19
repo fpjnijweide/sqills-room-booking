bea43a60013d8af84c35f347563395742610a51d
Platon Frolov <p.m.frolov@student.utwente.nl>
2019-06-19 09:57:05 +0200

    small changes

--- src/main/java/nl/utwente/servlet/desktopInterface/BookingServlet.java
+++ src/main/java/nl/utwente/servlet/desktopInterface/BookingServlet.java
@@ -1,7 +1,6 @@
 package nl.utwente.servlet.desktopInterface;
 
 import javax.servlet.ServletException;
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -10,21 +9,6 @@ import java.io.IOException;
 public class BookingServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
-        if (hasCookie(req.getCookies())){
-            req.getRequestDispatcher("/desktop/book.jsp").forward(req, res);
-        } else {
-            System.out.println("wf2g2");
-        }
-
-    }
-
-    public boolean hasCookie(Cookie[] cookies){
-        for (Cookie c : cookies){
-            if (c.getName().equals("fuck")){
-                System.out.println(c.getName());
-                return true;
-            }
-        }
-        return false;
+        req.getRequestDispatcher("/desktop/book.jsp").forward(req, res);
     }
 }
