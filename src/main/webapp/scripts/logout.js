7434c045d4a436db7e245d832dd3dadc38e899a7
Andrew <a.j.heath@student.utwente.nl>
2019-06-22 16:01:58 +0200

    Primitive, version of login in/out using google account

--- src/main/webapp/scripts/logout.js
+++ src/main/webapp/scripts/logout.js
@@ -1,6 +1,13 @@
 function logout(){
-    axios.post(`/api/user/logout`).then((response) => {
-        location.reload();
-
+    let auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+            axios.post(`/api/user/logout`).then((response) => {
+                location.reload();
+            });
     })
+}
+function onLoad() {
+    gapi.load('auth2', function () {
+        gapi.auth2.init();
+    });
 }
