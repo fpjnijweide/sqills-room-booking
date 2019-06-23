62c4e647badbd7fa41079bd386d69190f27b8d10
Andrew <a.j.heath@student.utwente.nl>
2019-06-23 17:29:45 +0200

    Google Auth scopes corrected, GC event are added to calendar on create booking on room pages only

--- src/main/java/nl/utwente/google/GoogleAuth.java
+++ src/main/java/nl/utwente/google/GoogleAuth.java
@@ -1,95 +1,36 @@
-//package nl.utwente.google;
-//
-//import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
-//import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
-//import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
-//import com.google.api.client.http.HttpTransport;
-//import com.google.api.client.json.JsonFactory;
-//import com.google.api.client.json.jackson2.JacksonFactory;
-//import com.google.gson.Gson;
-//import com.google.gson.JsonElement;
-//import com.google.gson.JsonObject;
-//import com.google.gson.JsonParser;
-//
-//import java.io.FileNotFoundException;
-//import java.io.FileReader;
-//import java.io.IOException;
-//import java.security.GeneralSecurityException;
-//import java.util.Collections;
-//
-//public class GoogleAuth {
-//
-//
-//    private static final String CLIENT_ID = ;
-//    HttpTransport httpTransport;
-//
-//    {
-//        try {
-//            httpTransport = GoogleNetHttpTransport.newTrustedTransport();
-//            JsonFactory jsonFactory = JacksonFactory.getDefaultInstance();
-//            Gson gson = new Gson();
-//            JsonParser parser = new JsonParser();
-//
-//            JsonElement jsontree = parser.parse(new FileReader("src/main/java/nl/utwente/keys/credentials.json"));
-//
-//            JsonObject je = jsontree.getAsJsonObject();
-//            System.out.println();
-//
-//
-//            GoogleIdTokenVerifier verifier = new GoogleIdTokenVerifier.Builder(httpTransport, jsonFactory)
-//                    // Specify the CLIENT_ID of the app that accesses the backend:
-//                    .setAudience(Collections.singletonList(je.getAsJsonObject("web").get("client_id"))
-//                    // Or, if multiple clients access the backend:
-//                    //.setAudience(Arrays.asList(CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3))
-//                    .build();
-//
-//            // (Receive idTokenString by HTTPS POST)
-//
-//            GoogleIdToken idToken = verifier.verify(idTokenString);
-//            if(idToken !=null)
-//
-//            {
-//                GoogleIdToken payload = idToken.getPayload();
-//
-//                // Print user identifier
-//                String userId = payload.getSubject();
-//                System.out.println("User ID: " + userId);
-//
-//                // Get profile information from payload
-//                String email = payload.getEmail();
-//                boolean emailVerified = Boolean.valueOf(payload.getEmailVerified());
-//                String name = (String) payload.get("name");
-//                String pictureUrl = (String) payload.get("picture");
-//                String locale = (String) payload.get("locale");
-//                String familyName = (String) payload.get("family_name");
-//                String givenName = (String) payload.get("given_name");
-//
-//                // Use or store profile information
-//                // ...
-//
-//            } else
-//
-//            {
-//                System.out.println("Invalid ID token.");
-//            }
-//        } catch (GeneralSecurityException e) {
-//            e.printStackTrace();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//    }
-//
-//
-//    public static void main(String[] args) throws FileNotFoundException {
-////        httpTransport = GoogleNetHttpTransport.newTrustedTransport();
-////        JsonFactory jsonFactory = JacksonFactory.getDefaultInstance();
-////        Gson gson = new Gson();
-//        JsonParser parser = new JsonParser();
-//
-//        JsonElement jsontree = parser.parse(new FileReader("src/main/java/nl/utwente/keys/credentials.json"));
-//
-//        JsonObject je = jsontree.getAsJsonObject();
-//        System.out.println(je.getAsJsonObject("web").get("client_id"));
-//    }
-//
-//}
+package nl.utwente.google;
+
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.util.Collections;
+
+public class GoogleAuth {
+
+
+    private static final String CLIENT_ID = "347026751328-n650qv0b0v1qjmmnk16vddsae05rqp4v.apps.googleusercontent.com";
+
+    public static GoogleIdTokenVerifier getVerifier() throws GeneralSecurityException, IOException {
+        HttpTransport  httpTransport = GoogleNetHttpTransport.newTrustedTransport();
+        JsonFactory jsonFactory = JacksonFactory.getDefaultInstance();
+        return new GoogleIdTokenVerifier.Builder(httpTransport, jsonFactory)
+                .setAudience(Collections.singletonList(CLIENT_ID)).build();
+    }
+
+
+    public static String getUser(GoogleIdToken idToken){
+        return idToken.getPayload().getEmail();
+    }
+
+    public static GoogleIdToken getToken(String tokenString) throws GeneralSecurityException, IOException {
+            return  getVerifier().verify(tokenString);
+    }
+
+
+}
--- src/main/webapp/desktop/login.jsp
+++ src/main/webapp/desktop/login.jsp
@@ -6,28 +6,26 @@
     <jsp:include page="head.jsp"/>
     <script src="/scripts/login.js"></script>
     <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
-    <script src="https://apis.google.com/js/platform.js" async defer></script>
-    <meta name="google-signin-client_id" content="47026751328-n650qv0b0v1qjmmnk16vddsae05rqp4v.apps.googleusercontent.com">
 </head>
 <body>
 <%--<jsp:include page="nav.jsp"/>--%>
 <div class="loginform">
     <table class="logintable">
-<%--<tr>--%>
-<%--    <td><div class="g-signin2" data-onsuccess="onSignIn"></div></td>--%>
-<%--</tr>--%>
-        <tr>
-            <td>Username</td>
-            <td><input type="username" id="username"></td>
+<tr>
+    <td><div class="g-signin2" data-onsuccess="onSignIn"></div></td>
+</tr>
+        <%--<tr>--%>
+            <%--<td>Username</td>--%>
+            <%--<td><input type="username" id="username"></td>--%>
+        <%--</tr>--%>
+        <%--<tr>--%>
+            <%--<td>Password</td>--%>
+            <%--<td><input type="password" id="password"></td>--%>
+        <%--</tr>--%>
         </tr>
-        <tr>
-            <td>Password</td>
-            <td><input type="password" id="password"></td>
-        </tr>
-        </tr>
-        <tr>
-            <td><button onclick="login()">Login</button> </td>
-        </tr>
+        <%--<tr>--%>
+            <%--<td><button onclick="login()">Login</button> </td>--%>
+        <%--</tr>--%>
     </table>
 </div>
 <div id="response"></div>
