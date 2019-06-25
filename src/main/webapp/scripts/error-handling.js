230051efe9f147d65964a9e452ec1f3a1efe4755
Platon Frolov <p.m.frolov@student.utwente.nl>
2019-06-25 10:04:53 +0200

    fornt end changes

--- src/main/webapp/scripts/error-handling.js
+++ src/main/webapp/scripts/error-handling.js
@@ -1,4 +1,10 @@
 function showError(message){
+    console.log(message)
     $("#error-text").text(message)
     $("#errorModal").modal('show');
+
+    if ($("#exampleModalLong").length > 0) {
+        $("#exampleModalLong").modal('hide');
+
+    }
 }
