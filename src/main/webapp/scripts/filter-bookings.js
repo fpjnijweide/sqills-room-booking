73515e9fb2a39b5a7b44a0e4d815153922f77783
Marten Voorberg <martenvoorberg@gmail.com>
2019-06-26 16:12:06 +0200

    Implement filter page

---
+++ src/main/webapp/scripts/filter-bookings.js
@@ -0,0 +1,39 @@
+function filterBookings() {
+    let params = {
+        "email": document.getElementById("filter-email").value,
+        "title": document.getElementById("filter-title").value,
+    };
+
+    if (document.getElementById("filter-startdate").value === '') {
+        params.startDate = "1970-01-01";
+    } else {
+        params.startDate = document.getElementById("filter-startdate").value;
+    }
+
+    if (document.getElementById("filter-enddate").value === '') {
+        params.endDate = "2050-01-01";
+    } else {
+        params.endDate = document.getElementById("filter-enddate").value;
+    }
+
+    console.log(params);
+
+    axios.get('/api/booking/filter', {"params": params})
+        .then(response => {
+            let bookings = response.data;
+            let tableBodyContainer = document.getElementById("bookings-table-body");
+            tableBodyContainer.innerHTML = "";
+            for (let i = 0; i < bookings.length; i++) {
+                let booking = bookings[i];
+                tableBodyContainer.innerHTML += `
+                <tr onclick="redirectToBookingPage(${booking.bookingid})" class="booking-row-clickable">
+                    <td>${booking.title}</td>
+                    <td>${booking.userName}</td>
+                    <td>${booking.date}</td>
+                    <td>${booking.startTime}</td>
+                    <td>${booking.endTime}</td>
+                </tr>
+                `
+            }
+        })
+}
