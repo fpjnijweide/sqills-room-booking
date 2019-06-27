7e14ab10d7138dbb11d747a40fa37ddc15f6a18c
s1857746 <f.p.j.nijweide@student.utwente.nl>
2019-06-27 20:07:41 +0200

    Started fixing database connection (final try)

--- src/main/java/nl/utwente/db/DatabaseConnectionFactory.java
+++ src/main/java/nl/utwente/db/DatabaseConnectionFactory.java
@@ -1,5 +1,8 @@
 package nl.utwente.db;
 
+import org.apache.tomcat.jdbc.pool.DataSource;
+import org.apache.tomcat.jdbc.pool.PoolProperties;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -9,37 +12,53 @@ import java.sql.SQLException;
  */
 public class DatabaseConnectionFactory {
 
-    public static Connection connection = getConnection();
+    static DataSource datasource;
+    static PoolProperties poolProperties;
+    static final String host = "castle.ewi.utwente.nl";
+    static final String dbName = "di125";
+    static final String url = "jdbc:postgresql://" + host + ":5432/" + dbName;
+    static final String username = "di125";
+    static final String password = "E9+gNMnM";
+
+    public DatabaseConnectionFactory() {
+        // We actually use a connection pool of 100 instead of randomly assigning connections
+        // (which can lead to problems when there are many users (or a DDOS attack or such)
+        // But this is better than using just one connection
+        poolProperties = new PoolProperties();
+        poolProperties.setUrl(url);
+        poolProperties.setDriverClassName("org.postgresql.Driver");
+        poolProperties.setUsername(username);
+        poolProperties.setPassword(password);
+        poolProperties.setJmxEnabled(true);
+        poolProperties.setTestWhileIdle(false);
+        poolProperties.setTestOnBorrow(true);
+        poolProperties.setValidationQuery("SELECT 1");
+        poolProperties.setTestOnReturn(false);
+        poolProperties.setValidationInterval(30000);
+        poolProperties.setTimeBetweenEvictionRunsMillis(30000);
+        poolProperties.setMaxActive(100);
+        poolProperties.setInitialSize(10);
+        poolProperties.setMaxWait(10000);
+        poolProperties.setRemoveAbandonedTimeout(60);
+        poolProperties.setMinEvictableIdleTimeMillis(30000);
+        poolProperties.setMinIdle(10);
+        poolProperties.setLogAbandoned(true);
+        poolProperties.setRemoveAbandoned(true);
+        poolProperties.setDefaultAutoCommit(false);
+        poolProperties.setJdbcInterceptors(
+            "org.apache.tomcat.jdbc.pool.interceptor.ConnectionState;"+
+                "org.apache.tomcat.jdbc.pool.interceptor.StatementFinalizer");
+        datasource = new DataSource();
+        datasource.setPoolProperties(poolProperties);
+    }
+
     /**
      * Returns a connection to the SQL database.
      * @return connection object to the database
      *         null if no connection could be established
      */
-    public static Connection getConnection(){
-        try {
-            Class.forName("org.postgresql.Driver");
-        } catch (ClassNotFoundException cnfe) {
-            System.err.println("Error loading driver: " + cnfe);
-        }
-        final String host = "castle.ewi.utwente.nl";
-        final String dbName = "di125";
-        final String url = "jdbc:postgresql://" + host + ":5432/" + dbName;
-        final String username = "di125";
-        final String password = "E9+gNMnM";
-        try {
-            Connection connection =  DriverManager.getConnection(url, username, password);
-            connection.setAutoCommit(false);
-            return connection;
-        } catch(SQLException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-    @Override
-    protected void finalize() throws Throwable {
-        connection.close();
-        super.finalize();
+    public static Connection getConnection() throws SQLException {
+        return datasource.getConnection();
     }
 
 }
